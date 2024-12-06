3bbda9ee9b07e9e197748a9548bfd6c1bd67a7c6
Hassan Hossam <egoker1234@gmail.com>
2024-12-06 12:36:20 +0200

    starting

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from './components/header/header'
+import { Video } from './components/video/video'
+import { Courses } from './components/courses/courses'
+import { Footer } from './components/footer/footer'
+function App() {
+
+  return (
+    <div className='App'>
+      <div className="app-container">
+        <Header />
+        {/* <Video />
+        <Courses />
+        <Footer /> */}
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import "header.scss"
+export const Header = () => {
+  return <div className="header-sec">
+  <h1></h1> 
+  <h1>Header</h1> 
+  <h2>Header</h2> 
+  <h3>Header</h3> 
+  <h4>Header</h4> 
+  <h5>Header</h5> 
+  </div>;
+};
